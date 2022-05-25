6d8d40af6b0b31796d203d1be32c8ea6163d2594
semen.tishchenko <semen.tishchenko@glowbyteconsulting.com>
2022-05-25 10:49:04 +0300

    feat: page 404

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
-const isAuth = true;
+// const isAuth = true;
 
-if (isAuth) {
-    history.replaceState({}, '', '/login.hbs')
-} else {
-    history.replaceState({}, '', '/app.hbs')
-}
-console.log('hellop from index.js')
+// if (isAuth) {
+//     history.replaceState({}, '', '/login.hbs')
+// } else {
+//     history.replaceState({}, '', '/app.hbs')
+// }
+// console.log('hellop from index.js')
